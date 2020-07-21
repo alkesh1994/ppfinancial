1d9e63e25349ae78ea2a1986ec941cccd3285119
Sunil Samel <sunilsamel93@gmail.com>
2020-07-21 21:09:07 +0530

    pdf export in progress

--- app/Http/Controllers/Client/PassbookController.php
+++ app/Http/Controllers/Client/PassbookController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Client\Client;
 use App\Models\Client\Account;
 use App\Models\Client\Passbook;
+use PDF;
 
 class PassbookController extends Controller
 {
@@ -21,4 +22,17 @@ class PassbookController extends Controller
     return view('dashboard.client.account.passbook.show',['account' => $account,'client'=> $client,'passbookEntries'=> $passbookEntries]);
 
   }
+
+  //export passbook as pdf
+  public function export_passbook_pdf($clientSlug,$accountSlug)
+  {
+    // Fetch all records from database
+    $account = Account::where('slug',$accountSlug)->firstOrFail();
+    $passbookEntries = Passbook::where('account_id',$account->id)->get();
+    // Send data to the view using loadView function of PDF facade
+    $pdf = PDF::loadView('dashboard.client.account.passbook.exportPassbookPdf', ['passbookEntries'=> $passbookEntries]);
+    // download the file using download function
+    return $pdf->download('passbook.pdf');
+  }
+
 }
